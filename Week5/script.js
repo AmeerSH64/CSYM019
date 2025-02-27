7a6377ca44b3863353e780e1f7a5c2d5c2ff3dd2
AmeerSH64 <ameer_7@live.co.uk>
2025-02-27 11:08:19 +0000

    Week 5 Exercise 5, 6 & 7

--- Week5/script.js
+++ Week5/script.js
@@ -1,14 +1,19 @@
-function myClickEvent() {
-    alert('The button was pressed');
+function myInterval() {
     var element = document.getElementById('circle');
-    element.style.backgroundColor = 'blue';
-    element.style.opacity = 1;
+    var circleOpacity = parseFloat(element.style.opacity);
+    element.style.opacity = circleOpacity - 0.01;
+}
+
+function myClickEvent() {    
+    var element = document.getElementById('circle');
+    var leftPosition = element.offsetLeft;
+    element.style.left = leftPosition - 10 + 'px';
 }
 
 function myLoadFunction() {
     var element = document.getElementById('circle');
-    element.addEventListener('click', myClickEvent);
-    element.style.opacity = 0.5;
+    element.addEventListener('keydown', myClickEvent);
+    element.style.opacity = 1;
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
