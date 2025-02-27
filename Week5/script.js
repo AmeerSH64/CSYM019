de7235f30b066fb4297811559bedc3b75c7e4c58
AmeerSH64 <ameer_7@live.co.uk>
2025-02-27 10:57:35 +0000

    Week 5 Exercise 3 & 4

--- Week5/script.js
+++ Week5/script.js
@@ -2,11 +2,13 @@ function myClickEvent() {
     alert('The button was pressed');
     var element = document.getElementById('circle');
     element.style.backgroundColor = 'blue';
+    element.style.opacity = 1;
 }
 
 function myLoadFunction() {
     var element = document.getElementById('circle');
     element.addEventListener('click', myClickEvent);
+    element.style.opacity = 0.5;
 }
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
